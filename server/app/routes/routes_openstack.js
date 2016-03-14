0a3f0b79e42555f39fddf08da448c8e8f65a845a
anshul srivastava <anshul_srivastava1987@yahoo.co.in>
2016-03-14 17:48:19 +0530

    fixed issued for CAT-1652

--- server/app/routes/routes_openstack.js
+++ server/app/routes/routes_openstack.js
@@ -25,239 +25,342 @@ var uuid = require('node-uuid');
 
 module.exports.setRoutes = function(app, verificationFunc) {
 
-    app.all('/openstack/*', verificationFunc);
+	app.all('/openstack/*', verificationFunc);
 
-    var getopenstackprovider = function(providerid, callback) {
-        var openstackconfig = {
-            host: "",
-            username: "",
-            password: "",
-            tenantName: "",
-            tenantId: "",
-            serviceendpoints: {}
-        };
+	var getopenstackprovider = function(providerid, callback) {
+		var openstackconfig = {
+			host: "",
+			username: "",
+			password: "",
+			tenantName: "",
+			tenantId: "",
+			serviceendpoints: {}
+		};
 
-        openstackProvider.getopenstackProviderById(providerid, function(err, data) {
-            logger.debug('IN getopenstackProviderById: data: ');
-            if (data) {
-                openstackconfig.host = data.host;
-                openstackconfig.username = data.username;
-                openstackconfig.password = data.password;
-                openstackconfig.tenantName = data.tenantname;
-                openstackconfig.tenantId = data.tenantid;
-                openstackconfig.serviceendpoints = data.serviceendpoints;
-                logger.debug('IN getopenstackProviderById: openstackconfig: ', JSON.stringify(openstackconfig));
-                callback(null, openstackconfig);
+		openstackProvider.getopenstackProviderById(providerid, function(err, data) {
+			logger.debug('IN getopenstackProviderById: data: ');
+			if (data) {
+				openstackconfig.host = data.host;
+				openstackconfig.username = data.username;
+				openstackconfig.password = data.password;
+				openstackconfig.tenantName = data.tenantname;
+				openstackconfig.tenantId = data.tenantid;
+				openstackconfig.serviceendpoints = data.serviceendpoints;
+				logger.debug('IN getopenstackProviderById: openstackconfig: ', JSON.stringify(openstackconfig));
+				callback(null, openstackconfig);
 
-            } else {
-                callback(null, null);
-            }
-        });
+			} else {
+				callback(null, null);
+			}
+		});
 
-    }
+	}
 
-    app.get('/openstack/:providerid/projects', function(req, res) {
+	app.get('/openstack/:providerid/projects', function(req, res) {
 
-        getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
+		getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
 
-            var openstack = new Openstack(openstackconfig);
+			var openstack = new Openstack(openstackconfig);
 
-            openstack.getProjects(function(err, projects) {
-                if (err) {
-                    logger.error('openstack tenants fetch error', err);
-                    res.status(500).send(err.error.message);
-                    return;
-                }
+			openstack.getProjects(function(err, projects) {
+				if (err) {
+					logger.error('openstack tenants fetch error', err);
+					res.status(500).send(err.error.message);
+					return;
+				}
 
-                res.send(projects);
-            });
-        });
-    });
+				res.send(projects);
+			});
+		});
+	});
 
 
-    app.get('/openstack/:providerid/tenants', function(req, res) {
+	app.get('/openstack/:providerid/tenants', function(req, res) {
 
-        getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
-            if (openstackconfig) {
-                var openstack = new Openstack(openstackconfig);
+		getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
+			if (openstackconfig) {
+				var openstack = new Openstack(openstackconfig);
 
-                openstack.getTenants(function(err, tenants) {
-                    if (err) {
-                        logger.error('openstack tenants fetch error', err);
-                        res.status(500).send(err.error.message);
-                        return;
-                    }
+				openstack.getTenants(function(err, tenants) {
+					if (err) {
+						logger.error('openstack tenants fetch error', err);
+						res.status(500).send(err.error.message);
+						return;
+					}
 
-                    res.send(tenants);
-                });
-            } else {
-                res.status(404).send("Provider not found");
-                return;
-            }
-        });
-    });
+					res.send(tenants);
+				});
+			} else {
+				res.status(404).send("Provider not found");
+				return;
+			}
+		});
+	});
 
-    app.get('/openstack/:providerid/images', function(req, res) {
+	app.get('/openstack/:providerid/images', function(req, res) {
 
-        getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
+		getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
 
-            var openstack = new Openstack(openstackconfig);
+			var openstack = new Openstack(openstackconfig);
 
-            openstack.getImages(openstackconfig.tenantid, function(err, images) {
-                if (err) {
-                    logger.error('openstack images fetch error', err);
-                    res.status(500).send(err.error.message);
-                    return;
-                }
+			openstack.getImages(openstackconfig.tenantid, function(err, images) {
+				if (err) {
+					logger.error('openstack images fetch error', err);
+					res.status(500).send(err.error.message);
+					return;
+				}
 
-                res.send(images);
-            });
-        });
-    });
+				res.send(images);
+			});
+		});
+	});
 
-    app.get('/openstack/:providerid/:tenantId/servers', function(req, res) {
+	app.get('/openstack/:providerid/:tenantId/servers', function(req, res) {
 
-        getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
+		getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
 
-            var openstack = new Openstack(openstackconfig);
+			var openstack = new Openstack(openstackconfig);
 
-            openstack.getServers(openstackconfig.tenantid, function(err, servers) {
-                if (err) {
-                    logger.error('openstack servers fetch error', err);
-                    res.status(500).send(err);
-                    return;
-                }
+			openstack.getServers(openstackconfig.tenantid, function(err, servers) {
+				if (err) {
+					logger.error('openstack servers fetch error', err);
+					res.status(500).send(err);
+					return;
+				}
 
-                res.send(servers);
-            });
-        });
-    });
+				res.send(servers);
+			});
+		});
+	});
 
-    app.get('/openstack/:providerid/networks', function(req, res) {
-        logger.debug('Inside openstack get networks:');
-        getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
+	app.get('/openstack/:providerid/networks', function(req, res) {
+		logger.debug('Inside openstack get networks:');
+		getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
 
-            var openstack = new Openstack(openstackconfig);
-            if (openstackconfig.serviceendpoints.network) {
-                openstack.getNetworks(function(err, networks) {
-                    if (err) {
-                        logger.error('openstack networks fetch error', err);
-                        res.status(500).send(err);
-                        return;
-                    }
-                    res.send(networks);
-                    logger.debug('Exit openstack get networks' + JSON.stringify(networks));
-                });
-            } else {
-                res.send([]);
-            }
-        });
-    });
+			var openstack = new Openstack(openstackconfig);
+			if (openstackconfig.serviceendpoints.network) {
+				openstack.getNetworks(function(err, networks) {
+					if (err) {
+						logger.error('openstack networks fetch error', err);
+						res.status(500).send(err);
+						return;
+					}
+					res.send(networks);
+					logger.debug('Exit openstack get networks' + JSON.stringify(networks));
+				});
+			} else {
+				res.send([]);
+			}
+		});
+	});
 
-    app.get('/openstack/:providerid/flavors', function(req, res) {
-        logger.debug('Inside openstack get flavors');
-        getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
+	app.get('/openstack/:providerid/networks/:networkId', function(req, res) {
+		logger.debug('Inside openstack get networks:');
+		getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
 
-            var openstack = new Openstack(openstackconfig);
+			var openstack = new Openstack(openstackconfig);
+			if (openstackconfig.serviceendpoints.network) {
+				openstack.getNetworks(function(err, networks) {
+					if (err) {
+						logger.error('openstack networks fetch error', err);
+						res.status(500).send(err);
+						return;
+					}
+					networks = networks.networks;
+					if (networks) {
+						for (var i = 0; i < networks.length; i++) {
+							if (networks[i].id === req.params.networkId) {
+								res.status(200).send(networks[i]);
+								return;
+							}
+						}
 
-            openstack.getFlavors(openstackconfig.tenantId, function(err, flavors) {
-                if (err) {
-                    logger.error('openstack flavors fetch error', err);
-                    res.status(500).send(err);
-                    return;
-                }
+					}
+					res.status(404).send({
+						message: "Network not found"
+					});
 
-                res.send(flavors);
-                logger.debug('Exit openstack get flavors' + JSON.stringify(flavors));
-            });
-        });
-    });
+				});
+			} else {
+				res.send([]);
+			}
+		});
+	});
 
-    app.get('/openstack/:providerid/securityGroups', function(req, res) {
-        getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
+	app.get('/openstack/:providerid/flavors', function(req, res) {
+		logger.debug('Inside openstack get flavors');
+		getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
 
-            var openstack = new Openstack(openstackconfig);
-            if (openstackconfig.serviceendpoints.network) {
-                openstack.getSecurityGroups(function(err, securityGroups) {
-                    if (err) {
-                        logger.error('openstack securityGroups fetch error', err);
-                        res.status(500).send(err);
-                        return;
-                    }
+			var openstack = new Openstack(openstackconfig);
 
-                    res.send(securityGroups);
-                    logger.debug('Exit openstack get securityGroups' + JSON.stringify(securityGroups));
-                });
-            } else {
-                res.send([]);
-            }
-        });
-    });
+			openstack.getFlavors(openstackconfig.tenantId, function(err, flavors) {
+				if (err) {
+					logger.error('openstack flavors fetch error', err);
+					res.status(500).send(err);
+					return;
+				}
 
-    app.post('/openstack/:providerid/:tenantId/createServer', function(req, res) {
-        var providerId = req.params.providerid;
-        getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
-            logger.debug('Openstackconfig', openstackconfig);
-            var openstack = new Openstack(openstackconfig);
-            VMImage.getImageByProviderId(providerId, function(err, images) {
-                if (err) {
-                    logger.error("Failed to fetch vmimages: ", err);
-                    res.status(500).send("Failed to fetch vmimages.");
-                    return;
-                }
-                if (images.length) {
-                    var opnstackBody = {
-                        server: {
-                            name: "server-test",
-                            imageRef: images[0].imageIdentifier,
-                            flavorRef: 2,
-                            max_count: 1,
-                            min_count: 1,
-                            networks: [{
-                                uuid: uuid.v4()
-                            }],
-                            security_groups: [{
-                                name: "default"
-                            }]
-                        }
-                    };
-                    openstack.createServer(openstackconfig.tenantId, JSON.stringify(opnstackBody.server), function(err, data) {
-                        if (err) {
-                            logger.error('openstack createServer error', err);
-                            res.status(500).send(err);
-                            return;
-                        }
+				res.send(flavors);
+				logger.debug('Exit openstack get flavors' + JSON.stringify(flavors));
+			});
+		});
+	});
 
-                        res.send(data);
-                        return;
-                    });
-                } else {
-                    res.status(404).send("Image not found.");
-                    return;
-                }
-            });
-        });
+	app.get('/openstack/:providerid/flavors/:flavorId', function(req, res) {
+		logger.debug('Inside openstack get flavors');
+		getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
 
-    });
+			var openstack = new Openstack(openstackconfig);
 
-    app.get('/openstack/:providerid/tenants/:tenantId/servers/:serverId', function(req, res) {
+			openstack.getFlavors(openstackconfig.tenantId, function(err, flavors) {
+				if (err) {
+					logger.error('openstack flavors fetch error', err);
+					res.status(500).send(err);
+					return;
+				}
 
-        getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
+				flavors = flavors.flavors;
+				if (flavors) {
+					for (var i = 0; i < flavors.length; i++) {
+						if (flavors[i].id === req.params.flavorId) {
+							res.status(200).send(flavors[i]);
+							return;
+						}
+					}
+				}
+				res.status(404).send({
+					message: "flavor not found"
+				});
 
-            var openstack = new Openstack(openstackconfig);
-            logger.debug("serverId:", req.params.serverId);
 
-            openstack.getServerById(req.params.tenantId, req.params.serverId, function(err, data) {
-                if (err) {
-                    logger.error('openstack createServer error', err);
-                    res.status(500).send(err);
-                    return;
-                }
+				res.send(flavors);
+				//logger.debug('Exit openstack get flavors' + JSON.stringify(flavors));
+			});
+		});
+	});
 
-                res.send(data);
-            });
 
-        });
-    });
+	app.get('/openstack/:providerid/securityGroups', function(req, res) {
+		getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
+
+			var openstack = new Openstack(openstackconfig);
+			if (openstackconfig.serviceendpoints.network) {
+				openstack.getSecurityGroups(function(err, securityGroups) {
+					if (err) {
+						logger.error('openstack securityGroups fetch error', err);
+						res.status(500).send(err);
+						return;
+					}
+
+					res.send(securityGroups);
+					logger.debug('Exit openstack get securityGroups' + JSON.stringify(securityGroups));
+				});
+			} else {
+				res.send([]);
+			}
+		});
+	});
+
+	app.get('/openstack/:providerid/securityGroups/:securityGroupId', function(req, res) {
+		getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
+
+			var openstack = new Openstack(openstackconfig);
+			if (openstackconfig.serviceendpoints.network) {
+				openstack.getSecurityGroups(function(err, securityGroups) {
+					if (err) {
+						logger.error('openstack securityGroups fetch error', err);
+						res.status(500).send(err);
+						return;
+					}
+                    //res.status(200).send(securityGroups);
+
+					securityGroups = securityGroups.security_groups;
+					if (securityGroups) {
+						for (var i = 0; i < securityGroups.length; i++) {
+							if (securityGroups[i].id === req.params.securityGroupId) {
+								res.status(200).send(securityGroups[i]);
+								return;
+							}
+						}
+					}
+					res.status(404).send({
+						message: "security group not found"
+					});
+
+
+
+				});
+			} else {
+				res.send([]);
+			}
+		});
+	});
+
+
+	app.post('/openstack/:providerid/:tenantId/createServer', function(req, res) {
+		var providerId = req.params.providerid;
+		getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
+			logger.debug('Openstackconfig', openstackconfig);
+			var openstack = new Openstack(openstackconfig);
+			VMImage.getImageByProviderId(providerId, function(err, images) {
+				if (err) {
+					logger.error("Failed to fetch vmimages: ", err);
+					res.status(500).send("Failed to fetch vmimages.");
+					return;
+				}
+				if (images.length) {
+					var opnstackBody = {
+						server: {
+							name: "server-test",
+							imageRef: images[0].imageIdentifier,
+							flavorRef: 2,
+							max_count: 1,
+							min_count: 1,
+							networks: [{
+								uuid: uuid.v4()
+							}],
+							security_groups: [{
+								name: "default"
+							}]
+						}
+					};
+					openstack.createServer(openstackconfig.tenantId, JSON.stringify(opnstackBody.server), function(err, data) {
+						if (err) {
+							logger.error('openstack createServer error', err);
+							res.status(500).send(err);
+							return;
+						}
+
+						res.send(data);
+						return;
+					});
+				} else {
+					res.status(404).send("Image not found.");
+					return;
+				}
+			});
+		});
+
+	});
+
+	app.get('/openstack/:providerid/tenants/:tenantId/servers/:serverId', function(req, res) {
+
+		getopenstackprovider(req.params.providerid, function(err, openstackconfig) {
+
+			var openstack = new Openstack(openstackconfig);
+			logger.debug("serverId:", req.params.serverId);
+
+			openstack.getServerById(req.params.tenantId, req.params.serverId, function(err, data) {
+				if (err) {
+					logger.error('openstack createServer error', err);
+					res.status(500).send(err);
+					return;
+				}
+
+				res.send(data);
+			});
+
+		});
+	});
 
 }
