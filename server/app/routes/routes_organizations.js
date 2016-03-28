22c8a6c7246cfd16292850bce1aa14e47bc6a8bb
Vinod Nair <vinod.nair@relevancelab.com>
2016-03-28 09:31:59 +0530

    Intermediate

--- server/app/routes/routes_organizations.js
+++ server/app/routes/routes_organizations.js
@@ -716,6 +716,11 @@ module.exports.setRoutes = function(app, sessionVerification) {
 		var blueprintType = req.body.blueprintData.blueprintType;
         var nexus = req.body.blueprintData.nexus;
         var docker = req.body.blueprintData.docker;
+<<<<<<< HEAD
+=======
+        var region = req.body.blueprintData.region;
+        var blueprintId = req.body.blueprintData.blueprintId;
+>>>>>>> BP Edit save done. UI pending
 
 		// a temp fix for invalid appurl data. will be removed in next iteration
 		var tempAppUrls = [];
@@ -761,6 +766,9 @@ module.exports.setRoutes = function(app, sessionVerification) {
                 nexus: nexus,
                 docker: docker
 			};
+			//adding bluerpintID if present (edit mode)
+			if(blueprintId)
+				blueprintData.id = blueprintId;
 
 			logger.debug('req blueprintData:', blueprintData);
 			var dockerData, instanceData;
