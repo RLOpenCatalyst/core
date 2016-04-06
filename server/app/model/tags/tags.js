a4059088f0def740bfc94ec597e388fa677eda1d
Karthik K N <karthik.nataraj@relevancelab.com>
2016-04-06 07:01:31 +0530

    tags aggregation cron job updated

--- server/app/model/tags/tags.js
+++ server/app/model/tags/tags.js
@@ -32,7 +32,8 @@ var TagSchema = new Schema({
     name: {
         type: String,
         trim: true,
-        required: true
+        required: true,
+        index: { unique: true }
     },
     values: {
         type: [String],
@@ -46,6 +47,7 @@ var TagSchema = new Schema({
     },
     catalystEntityType: {
         type: String,
+        enum: ['project', 'environment'],
         trim: true,
         required: false
     },
@@ -75,9 +77,13 @@ TagSchema.statics.createNew = function createNew(data, callback) {
     tags.save(function (err, data) {
         if (err) {
             logger.error(err);
-            return callback(err, null);
+            if(typeof callback == 'function') {
+                return callback(err, null);
+            }
         } else {
-            return callback(null, tags);
+            if(typeof callback == 'function') {
+                return callback(null, tags);
+            }
         }
     });
 }
@@ -160,10 +166,12 @@ TagSchema.statics.updateTag = function updateTag(params, fields, callback) {
         function(err, result) {
             if (err) {
                 logger.error(err);
-                return callback(err, null);
-            } else if(result.ok == 1 && result.n == 1) {
+                if(typeof callback == 'function') {
+                    return callback(err, null);
+                }
+            } else if((result.ok == 1 && result.n == 1) && (typeof callback == 'function'))  {
                 return callback(null, true);
-            } else {
+            } else if(typeof callback == 'function') {
                return callback(null, null);
             }
         }
@@ -184,6 +192,21 @@ TagSchema.statics.deleteTag = function deleteTag(params, callback) {
        }
    )
 };
+
+TagSchema.statics.updateTagValues = function updateTagValues(params, values, callback) {
+    this.update(
+        params,
+        {$push: {$each: {values: value}}},
+        function(err, tags) {
+            if(err) {
+                logger.error(err);
+                return callback(err, null);
+            } else {
+                return callback(null, true);
+            }
+        }
+    )
+}
 
 var Tags = mongoose.model('Tags',TagSchema);
 module.exports = Tags;
