a76c67a23fb4976846470fa6aced3e950585c32f
Divakar Konakalla <divakar.konakalla@relevancelab.com>
2016-04-20 16:37:49 +0530

    indented js files, removed extra lines & white spaces in between lines of code.

--- client/cat3/src/partials/sections/dashboard/workzone/instance/manage/templates/cpServicesCtrl.js
+++ client/cat3/src/partials/sections/dashboard/workzone/instance/manage/templates/cpServicesCtrl.js
@@ -8,84 +8,75 @@
 (function () {
     "use strict";
     angular.module('workzone.instance')
-            .controller('cpServicesCtrl', ['$scope', 'workzoneServices', '$modal', 'instanceFactories', function ($scope, workzoneServices, $modal, instanceFactories) {
-                    var cpInstance = $scope.$parent.cpInstance;
-                    $scope.instInfo = cpInstance;
-                    var helper = {
-                        doServiceAction: function (inst, service, actionType) {
-                            workzoneServices.getDoServiceActionOnInstance(inst._id, service.rowid, actionType).then(function () {
-                                helper.showActionHistory();
-                            }, function () {
-                                helper.showActionHistory();
-                            });
-                        },
-                        doServiceDelete: function (inst, service, idx) {
-                            workzoneServices.deleteServiceOnInstance(inst._id, service.rowid).then(
-                                    function () {
-                                        $scope.serviceInfo.splice(idx, 1);
-                                    },
-                                    function () {}
-                            );
-                        },
-                        showActionHistory: function () {
-                            $scope.$parent.activateTab('Logs');
-                        }
-                    };
-
+        .controller('cpServicesCtrl', ['$scope', 'workzoneServices', '$modal', 'instanceFactories', function ($scope, workzoneServices, $modal, instanceFactories) {
+            var cpInstance = $scope.$parent.cpInstance;
+            $scope.instInfo = cpInstance;
+            var helper = {
+                doServiceAction: function (inst, service, actionType) {
+                    workzoneServices.getDoServiceActionOnInstance(inst._id, service.rowid, actionType).then(function () {
+                        helper.showActionHistory();
+                    }, function () {
+                        helper.showActionHistory();
+                    });
+                },
+                doServiceDelete: function (inst, service, idx) {
+                    workzoneServices.deleteServiceOnInstance(inst._id, service.rowid).then(
+                            function () {
+                                $scope.serviceInfo.splice(idx, 1);
+                            },
+                            function () {}
+                    );
+                },
+                showActionHistory: function () {
+                    $scope.$parent.activateTab('Logs');
+                }
+            };
+            $scope.serviceInfo = [];
+            $scope.setSerivceInfo = function () {
+                if (cpInstance.serviceIds && cpInstance.serviceIds.length) {
+                    workzoneServices.postRetrieveServiceDetails(cpInstance.serviceIds).then(function (response) {
+                        $scope.serviceInfo = response.data;
+                        $scope.serviceInfo = instanceFactories.getAllServiceActionItems($scope.serviceInfo);
+                    }, function () {
+                        alert('An error occurred while getting service list');
+                    });
+                } else {
                     $scope.serviceInfo = [];
-                    $scope.setSerivceInfo = function () {
-                        if (cpInstance.serviceIds && cpInstance.serviceIds.length) {
-                            workzoneServices.postRetrieveServiceDetails(cpInstance.serviceIds).then(function (response) {
-                                $scope.serviceInfo = response.data;
-                                $scope.serviceInfo = instanceFactories.getAllServiceActionItems($scope.serviceInfo);
-                            }, function () {
-                                alert('An error occurred while getting service list');
-                            });
-                        } else {
-                            $scope.serviceInfo = [];
+                }
+            };
+            $scope.setSerivceInfo();
+            $scope.serviceAction = function (service, action) {
+                helper.doServiceAction(cpInstance, service, action);
+            };
+            $scope.deleteService = function (service, index) {
+                helper.doServiceDelete(cpInstance, service, index);
+            };
+            $scope.addNewService = function () {
+                $modal.open({
+                    animation: true,
+                    templateUrl: 'src/partials/sections/dashboard/workzone/instance/manage/popups/addNewService.html',
+                    controller: 'addNewServiceCtrl',
+                    backdrop: 'static',
+                    keyboard: false,
+                    resolve: {
+                        dataObj: function () {
+                            return {
+                                serviceIds: cpInstance.serviceIds,
+                                inspectInstance: cpInstance
+                            };
                         }
-                    };
+                    }
+                }).result.then(function () {
                     $scope.setSerivceInfo();
-
-                    $scope.serviceAction = function (service, action) {
-                        helper.doServiceAction(cpInstance, service, action);
-                    };
-                    $scope.deleteService = function (service, index) {
-                        helper.doServiceDelete(cpInstance, service, index);
-                    };
-
-                    $scope.addNewService = function () {
-                        $modal.open({
-                            animation: true,
-                            templateUrl: 'src/partials/sections/dashboard/workzone/instance/manage/popups/addNewService.html',
-                            controller: 'addNewServiceCtrl',
-                            backdrop: 'static',
-                            keyboard: false,
-                            resolve: {
-                                dataObj: function () {
-                                    return {
-                                        serviceIds: cpInstance.serviceIds,
-                                        inspectInstance: cpInstance
-                                    };
-                                }
-                            }
-                        }).result.then(function () {
-                            $scope.setSerivceInfo();
-                        }, function () {
-                            console.log('Modal Dismissed at ' + new Date());
-                        });
-                    };
-
-                }]).controller('addNewServiceCtrl', ['$scope', '$modalInstance', 'workzoneServices', 'dataObj', 'cacheServices', 'instanceFactories', function ($scope, $modalInstance, workzoneServices, dataObj, cacheServices, instanceFactories) {
-
+                }, function () {
+                    console.log('Modal Dismissed at ' + new Date());
+                });
+            };
+        }]).controller('addNewServiceCtrl', ['$scope', '$modalInstance', 'workzoneServices', 'dataObj', 'cacheServices', 'instanceFactories', function ($scope, $modalInstance, workzoneServices, dataObj, cacheServices, instanceFactories) {
             $scope.serviceIds = dataObj.serviceIds;
-
             var cpInstance = dataObj.inspectInstance;
-
             var cacheKey = 'chefServerServices_' + cpInstance.chef.serverId;
-
             var services = cacheServices.getFromCache(cacheKey);
-
             if (services) {
                 $scope.serviceInfo = services;
             } else {
@@ -109,24 +100,19 @@
                     alert('An error occurred while getting service list');
                 });
             }
-
             $scope.selection = [];
-
             // toggle selection for a given Service by name
             $scope.toggleSelection = function (serviceId) {
                 var idx = $scope.selection.indexOf(serviceId);
-
                 // is currently selected
                 if (idx > -1) {
                     $scope.selection.splice(idx, 1);
                 }
-
                 // is newly selected
                 else {
                     $scope.selection.push(serviceId);
                 }
             };
-
             $scope.cancel = function () {
                 $modalInstance.dismiss('cancel');
             };
@@ -139,5 +125,6 @@
                     $modalInstance.close();
                 });
             };
-        }]);
+        }
+    ]);
 })();
