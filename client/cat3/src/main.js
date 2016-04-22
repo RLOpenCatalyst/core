2dd161f69f80cc6ed96ae34d72578057d4126c55
hrushikesh07 <hrushikesh.das@relevance.com>
2016-04-22 16:09:29 +0530

    Added the pipeline card view and hisroty in the application tab

--- client/cat3/src/main.js
+++ client/cat3/src/main.js
@@ -28,6 +28,7 @@ var angularApp = angular.module('catapp', ['ui.router',
     'ui.grid',
     'ui.grid.pagination',
     'ui.grid.resizeColumns',
+    'ui.grid.cellNav'
 ]);
 
 angularApp.run(['$rootScope', 'auth', '$state', '$stateParams',
