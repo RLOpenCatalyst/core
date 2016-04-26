38f7bf4e3b3656fdcafcdd59870b4e846b000d72
revinjacob <revin.jacob@relevancelab.com>
2016-04-26 19:23:39 +0530

    Warning message when no environments found in workzone

---
+++ client/cat3/src/partials/globals/constants/constantsModule.js
@@ -0,0 +1,13 @@
+/* Copyright (C) Relevance Lab Private Limited- All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Relevance UI Team,
+ * Aug 2015
+ */
+
+(function(angular){
+	"use strict";
+	angular.module('global.constants',[])
+	.constant("NO_ENV_CONFIGURED", "No environments configured!");
+
+})(angular);
