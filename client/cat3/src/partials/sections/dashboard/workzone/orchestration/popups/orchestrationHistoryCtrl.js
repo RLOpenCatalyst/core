4d9393841778cfe760c9d4db97ea0a0ef2bf2486
arabind <arabindbapu@gmail.com>
2016-05-08 17:09:55 +0530

    Added UI Grid for chef Task in orchestration history

--- client/cat3/src/partials/sections/dashboard/workzone/orchestration/popups/orchestrationHistoryCtrl.js
+++ client/cat3/src/partials/sections/dashboard/workzone/orchestration/popups/orchestrationHistoryCtrl.js
@@ -8,8 +8,59 @@
 (function(){
 	"use strict";
 	angular.module('workzone.orchestration')
-		.controller('orchestrationHistoryCtrl',["items",'$scope','$modalInstance','workzoneServices','$modal',
-			function(items,$scope,$modalInstance,workzoneServices,$modal){
+		.controller('orchestrationHistoryCtrl',["items", '$scope', '$modalInstance', '$modal', '$timeout', 'uiGridOptionsClient', 'uiGridConstants', 'workzoneServices',
+			function(items, $scope, $modalInstance, $modal, $timeout, uiGridOptionsClient, uiGridConstants, workzoneServices){
+
+				//UI Grid for chef Task starts
+				$scope.taskHistoryChefData = [];
+				var gridOptions = uiGridOptionsClient.options().gridOption;
+				$scope.taskHistoryChefGridOptions = gridOptions;
+				$scope.ischefTaskHistoryPageLoading = true;
+
+				$scope.initChefGrids = function(){
+					$scope.taskHistoryChefGridOptions.data='taskHistoryChefData';
+					$scope.taskHistoryChefGridOptions.columnDefs = [
+					{ name:'Start Time',cellTemplate:'<span title="{{row.entity.timestampStarted  | timestampToLocaleTime}}">{{row.entity.timestampStarted  | timestampToLocaleTime}}</span>',cellTooltip: true, sort: { direction: 'uiGridConstants.ASC' }},
+					{ name:'End Time',cellTemplate:'<span title="{{row.entity.timestampEnded  | timestampToLocaleTime}}">{{row.entity.timestampEnded  | timestampToLocaleTime}}</span>',cellTooltip: true},
+					// { name:'Status',field:'status',cellClass:'success'},
+					{ name:'Status',field:'status',cellTemplate:'<div class="{{row.entity.status}}">{{row.entity.status}}</div>',sort: { direction: 'uiGridConstants.ASC' }},
+					{ name:'Message', field: 'message', 
+					  cellTemplate:'<span title="{{row.entity.message}}">{{row.entity.message}}</span>'},
+					{ name:'User',field:'user',cellTooltip: true},
+					{ name:'Logs',
+					  cellTemplate:'<div class="text-center"><i class="fa fa-info-circle cursor" title="More Info" ng-click="grid.appScope.historyLogs(row.entity)"></i></div>'}
+					];
+				};
+				angular.extend($scope, {
+					taskHistoryChefListView: function() {
+						$scope.taskHistoryChefData = [];
+						workzoneServices.getHistory(items._id).then(function(response) {
+							$timeout(function() {
+								if(response.data){
+									$scope.taskHistoryChefData = response.data;
+									$scope.ischefTaskHistoryPageLoading = false;
+								}else if(response){
+									$scope.taskHistoryChefData = response;
+									$scope.ischefTaskHistoryPageLoading = false;
+								}
+							},100);
+						}, function(error){
+							$scope.errorMessage = "No Chef History Records found";
+						});
+					},
+				});
+				$scope.initchef = function(){
+					$scope.initChefGrids();
+					$scope.taskHistoryChefListView();
+				};
+				$scope.initchef();
+				//UI Grid for chef Task ends
+
+
+
+
+
+
 				$scope.task=items;
 				workzoneServices.getHistory(items._id).then(function(response) {
 					var data;
