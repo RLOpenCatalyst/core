d241397b49f3a297ea6a6c86e0467140ef7121b1
arabind <arabindbapu@gmail.com>
2016-05-09 16:19:50 +0530

    Added UI grid for Orchestration history pages

--- client/cat3/src/partials/sections/dashboard/workzone/orchestration/popups/orchestrationHistoryCtrl.js
+++ client/cat3/src/partials/sections/dashboard/workzone/orchestration/popups/orchestrationHistoryCtrl.js
@@ -15,14 +15,12 @@
 				$scope.taskHistoryChefData = [];
 				var gridOptions = uiGridOptionsClient.options().gridOption;
 				$scope.taskHistoryChefGridOptions = gridOptions;
-				$scope.ischefTaskHistoryPageLoading = true;
 
 				$scope.initChefGrids = function(){
 					$scope.taskHistoryChefGridOptions.data='taskHistoryChefData';
 					$scope.taskHistoryChefGridOptions.columnDefs = [
 					{ name:'Start Time',cellTemplate:'<span title="{{row.entity.timestampStarted  | timestampToLocaleTime}}">{{row.entity.timestampStarted  | timestampToLocaleTime}}</span>',cellTooltip: true, sort: { direction: 'uiGridConstants.ASC' }},
 					{ name:'End Time',cellTemplate:'<span title="{{row.entity.timestampEnded  | timestampToLocaleTime}}">{{row.entity.timestampEnded  | timestampToLocaleTime}}</span>',cellTooltip: true},
-					// { name:'Status',field:'status',cellClass:'success'},
 					{ name:'Status',field:'status',cellTemplate:'<div class="{{row.entity.status}}">{{row.entity.status}}</div>',sort: { direction: 'uiGridConstants.ASC' }},
 					{ name:'Message', field: 'message', 
 					  cellTemplate:'<span title="{{row.entity.message}}">{{row.entity.message}}</span>'},
@@ -53,24 +51,166 @@
 					$scope.initChefGrids();
 					$scope.taskHistoryChefListView();
 				};
-				$scope.initchef();
 				//UI Grid for chef Task ends
 
+				//UI Grid for jenkins Task starts
+				$scope.taskHistoryJenkinsData = [];
+				var gridOptions = uiGridOptionsClient.options().gridOption;
+				$scope.taskHistoryJenkinsGridOptions = gridOptions;
+
+				$scope.initJenkinsGrids = function(){
+					$scope.taskHistoryJenkinsGridOptions.data='taskHistoryJenkinsData';
+					$scope.taskHistoryJenkinsGridOptions.columnDefs = [
+					{ name:'Job Number',cellTemplate:'<a target="_blank" title="Jenkins" ng-href="{{grid.appScope.task.taskConfig.jobURL}}/{{row.entity.buildNumber}}">{{row.entity.buildNumber}}</a>',cellTooltip: true},
+					{ name:'Job Output',cellTemplate:'<span title="Jenkins Log" class="fa fa-list bigger-120 btn cat-btn-update btn-sg tableactionbutton" ng-click="grid.appScope.historyLogs(row.entity);"></span>',cellTooltip: true},
+					{ name:'Status',field:'status',cellTemplate:'<div class="{{row.entity.status}}">{{row.entity.status}}</div>'},
+					{ name:'Start Time',cellTemplate:'<span title="{{row.entity.timestampStarted  | timestampToLocaleTime}}">{{row.entity.timestampStarted  | timestampToLocaleTime}}</span>',cellTooltip: true},
+					{ name:'End Time',cellTemplate:'<span title="{{row.entity.timestampEnded  | timestampToLocaleTime}}">{{row.entity.timestampEnded  | timestampToLocaleTime}}</span>',cellTooltip: true}
+					];
+				};
+				angular.extend($scope, {
+					taskHistoryJenkinsListView: function() {
+						$scope.taskHistoryJenkinsData = [];
+						workzoneServices.getHistory(items._id).then(function(response) {
+							$timeout(function() {
+								if(response.data){
+									$scope.taskHistoryJenkinsData = response.data;
+									$scope.isjenkinsTaskHistoryPageLoading = false;
+								}else if(response){
+									$scope.taskHistoryJenkinsData = response;
+									$scope.isjenkinsTaskHistoryPageLoading = false;
+								}
+							},100);
+						}, function(error){
+							$scope.errorMessage = "No Jenkins History Records found";
+						});
+					},
+				});
+				$scope.initjenkins = function(){
+					$scope.initJenkinsGrids();
+					$scope.taskHistoryJenkinsListView();
+				};
+				//UI Grid for jenkins Task ends
 
 
+				//UI Grid for composite Task starts
+				$scope.taskHistoryCompositeData = [];
+				var gridOptions = uiGridOptionsClient.options().gridOption;
+				$scope.taskHistoryCompositeGridOptions = gridOptions;
 
+				$scope.initCompositeGrids = function(){
+					$scope.taskHistoryCompositeGridOptions.data='taskHistoryCompositeData';
+					$scope.taskHistoryCompositeGridOptions.columnDefs = [
+					{ name:'Start Time',cellTemplate:'<span title="{{row.entity.timestampStarted  | timestampToLocaleTime}}">{{row.entity.timestampStarted  | timestampToLocaleTime}}</span>',cellTooltip: true, sort: { direction: 'uiGridConstants.ASC' }},
+					{ name:'End Time',cellTemplate:'<span title="{{row.entity.timestampEnded  | timestampToLocaleTime}}">{{row.entity.timestampEnded  | timestampToLocaleTime}}</span>',cellTooltip: true},
+					{ name:'Status',field:'status',cellTemplate:'<div class="{{row.entity.status}}">{{row.entity.status}}</div>',sort: { direction: 'uiGridConstants.ASC' }},
+					{ name:'Message', field: 'message', 
+					  cellTemplate:'<span title="{{row.entity.message}}">{{row.entity.message}}</span>'},
+					{ name:'User',field:'user',cellTooltip: true},
+					{ name:'Logs',
+					  cellTemplate:'<div class="text-center"><i class="fa fa-info-circle cursor" title="More Info" ng-click="grid.appScope.historyLogs(row.entity)"></i></div>'}
+					];
+				};
+				angular.extend($scope, {
+					taskHistoryCompositeListView: function() {
+						$scope.taskHistoryCompositeData = [];
+						workzoneServices.getHistory(items._id).then(function(response) {
+							$timeout(function() {
+								if(response.data){
+									$scope.taskHistoryCompositeData = response.data;
+									$scope.iscompositeTaskHistoryPageLoading = false;
+								}else if(response){
+									$scope.taskHistoryCompositeData = response;
+									$scope.iscompositeTaskHistoryPageLoading = false;
+								}
+							},100);
+						}, function(error){
+							$scope.errorMessage = "No Composite History Records found";
+						});
+					},
+				});
+				$scope.initcomposite = function(){
+					$scope.initCompositeGrids();
+					$scope.taskHistoryCompositeListView();
+				};
+				//UI Grid for composite Task ends
+
+
+				//UI Grid for puppet Task starts
+				$scope.taskHistoryPuppetData = [];
+				var gridOptions = uiGridOptionsClient.options().gridOption;
+				$scope.taskHistoryPuppetGridOptions = gridOptions;
+
+				$scope.initPuppetGrids = function(){
+					$scope.taskHistoryPuppetGridOptions.data='taskHistoryPuppetData';
+					$scope.taskHistoryPuppetGridOptions.columnDefs = [
+					{ name:'Start Time',cellTemplate:'<span title="{{row.entity.timestampStarted  | timestampToLocaleTime}}">{{row.entity.timestampStarted  | timestampToLocaleTime}}</span>',cellTooltip: true, sort: { direction: 'uiGridConstants.ASC' }},
+					{ name:'End Time',cellTemplate:'<span title="{{row.entity.timestampEnded  | timestampToLocaleTime}}">{{row.entity.timestampEnded  | timestampToLocaleTime}}</span>',cellTooltip: true},
+					{ name:'Status',field:'status',cellTemplate:'<div class="{{row.entity.status}}">{{row.entity.status}}</div>',sort: { direction: 'uiGridConstants.ASC' }},
+					{ name:'Message', field: 'message', 
+					  cellTemplate:'<span title="{{row.entity.message}}">{{row.entity.message}}</span>'},
+					{ name:'User',field:'user',cellTooltip: true},
+					{ name:'Logs',
+					  cellTemplate:'<div class="text-center"><i class="fa fa-info-circle cursor" title="More Info" ng-click="grid.appScope.historyLogs(row.entity)"></i></div>'}
+					];
+				};
+				angular.extend($scope, {
+					taskHistoryPuppetListView: function() {
+						$scope.taskHistoryPuppetData = [];
+						workzoneServices.getHistory(items._id).then(function(response) {
+							$timeout(function() {
+								if(response.data){
+									$scope.taskHistoryPuppetData = response.data;
+									$scope.ispuppetTaskHistoryPageLoading = false;
+								}else if(response){
+									$scope.taskHistoryPuppetData = response;
+									$scope.ispuppetTaskHistoryPageLoading = false;
+								}
+							},100);
+						}, function(error){
+							$scope.errorMessage = "No Puppet History Records found";
+						});
+					},
+				});
+				$scope.initpuppet = function(){
+					$scope.initPuppetGrids();
+					$scope.taskHistoryPuppetListView();
+				};
+				//UI Grid for puppet Task ends
 
 
 				$scope.task=items;
-				workzoneServices.getHistory(items._id).then(function(response) {
-					var data;
-					if(response.data){
-						data=response.data;
-					}else if(response){
-						data=response;
-					}
-					$scope.history=data;
-				});
+
+				switch ($scope.task.taskType){
+					case 'chef' :
+						$scope.ischefTaskHistoryPageLoading = true;
+						$scope.isjenkinsTaskHistoryPageLoading = false;
+						$scope.iscompositeTaskHistoryPageLoading = false;
+						$scope.ispuppetTaskHistoryPageLoading = false;
+						$scope.initchef();
+					break;
+					case 'jenkins' :
+						$scope.ischefTaskHistoryPageLoading = false;
+						$scope.isjenkinsTaskHistoryPageLoading = true;
+						$scope.iscompositeTaskHistoryPageLoading = false;
+						$scope.ispuppetTaskHistoryPageLoading = false;
+						$scope.initjenkins();
+					break;
+					case 'composite' :
+						$scope.ischefTaskHistoryPageLoading = false;
+						$scope.isjenkinsTaskHistoryPageLoading = false;
+						$scope.iscompositeTaskHistoryPageLoading = true;
+						$scope.ispuppetTaskHistoryPageLoading = false;
+						$scope.initcomposite();
+					break;
+					case 'puppet':
+						$scope.ischefTaskHistoryPageLoading = false;
+						$scope.isjenkinsTaskHistoryPageLoading = false;
+						$scope.iscompositeTaskHistoryPageLoading = false;
+						$scope.ispuppetTaskHistoryPageLoading = true;
+						$scope.initpuppet();
+					break;
+				}
 
 				$scope.historyLogs=function(hist) {
 					var modalInstance = $modal.open({
