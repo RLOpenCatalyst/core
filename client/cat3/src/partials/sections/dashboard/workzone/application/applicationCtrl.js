ffc95bdd58c329268cf151c05a608f81eb7d008a
srikanthv02 <srikanthv02@gmail.com>
2016-06-01 20:10:09 +0530

    Resolved issue related to Job Links.

--- client/cat3/src/partials/sections/dashboard/workzone/application/applicationCtrl.js
+++ client/cat3/src/partials/sections/dashboard/workzone/application/applicationCtrl.js
@@ -315,6 +315,7 @@
 				}
 			});
 			function removeSelect(){
+				$scope.pipeLineActBarShow =false;
 				$rootScope.selectedCardClass='';
 				$scope.currentTargetId='';
 				angular.element('.card').removeClass('selected-card');
