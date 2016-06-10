fe36c631cdb94632593f7a31551f7b8ea2e92c2a
Gobinda Das <gobinda.das@relevancelab.com>
2016-06-10 12:21:16 +0530

    Default pagination side set to 100000

--- server/install.js
+++ server/install.js
@@ -94,8 +94,8 @@ function getDefaultsConfig() {
                 "resources":"createdOn"
             },
             skip_Records : 1,
-            max_record_limit : 11000,
-            record_limit : 11000
+            max_record_limit : 100000,
+            record_limit : 100000
         },
         puppet: {
             puppetReposDirName: 'puppet-repos',
