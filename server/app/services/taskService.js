fde9630e017a65bd4ab1ccd25c1c38942d95597a
Gobinda Das <gobinda.das@relevancelab.com>
2016-06-27 18:18:37 +0530

    Code to fetch tasks for log trail

--- server/app/services/taskService.js
+++ server/app/services/taskService.js
@@ -16,6 +16,10 @@
 
 var logger = require('_pr/logger')(module);
 var taskDao = require('_pr/model/classes/tasks/tasks.js');
+var masterUtil = require('_pr/lib/utils/masterUtil.js');
+var d4dModelNew = require('_pr/model/d4dmasters/d4dmastersmodelnew.js');
+var TaskHistory = require('_pr/model/classes/tasks/taskHistory');
+var instancesDao = require('_pr/model/classes/instance/instance');
 
 const errorType = 'taskService';
 
@@ -75,7 +79,7 @@ taskService.getChefTasksByOrgBgProjectAndEnvId = function getChefTasksByOrgBgPro
 };
 
 taskService.executeTask = function executeTask(taskId, user, hostProtocol, choiceParam, appData, callback) {
-    if(appData){
+    if (appData) {
         appData['taskId'] = taskId;
     }
     taskDao.getTaskById(taskId, function(err, task) {
@@ -106,6 +110,78 @@ taskService.executeTask = function executeTask(taskId, user, hostProtocol, choic
             var err = new Error('Task Not Found.');
             err.status = 404;
             return callback(err, null);
+        }
+    });
+};
+
+taskService.taskList = function taskList(callback) {
+    taskDao.listTasks(function(err, tasks) {
+        if (err) {
+            res.status(500).send(errorResponses.db.error);
+            return;
+        }
+        var tList = [];
+        var instanceIds =[];
+        if (tasks && tasks.length) {
+            for (var i = 0; i < tasks.length; i++) {
+                d4dModelNew.d4dModelMastersProjects.find({
+                    rowid: tasks[i].projectId,
+                    id: '4'
+                }, function(err, projectData) {
+                    if (err) {
+                        logger.debug("getProjectsById: " + err);
+                        return callback(err, null);
+                    }
+                    if (projectData && projectData.length) {
+                        tasks[i]['orgName'] = projectData[0].orgname[0];
+                        tasks[i]['bgName'] = projectData[0].productgroupname;
+                        tasks[i]['projectName'] = projectData[0].projectname;
+                        d4dModelNew.d4dModelMastersEnvironments.find({
+                            rowid: tasks[i].envId,
+                            id: '3'
+                        }, function(err, envData) {
+                            if (err) {
+                                logger.debug("getEnvById: " + err);
+                                return callback(err, null);
+                            }
+                            if (envData && envData.length) {
+                                tasks[i]['envName'] = envData[0].environmentname;
+                                if (tasks[i].taskConfig && tasks[i].taskConfig.nodeIds && tasks[i].taskConfig.nodeIds.length) {
+                                    instancesDao.getInstances(tasks[i].taskConfig.nodeIds,function(err,instances){
+                                        if(err){
+                                            logger.error("Error while fetching instance: ",err);
+                                        }
+                                        if(instances && instances.length){
+                                            for(var j=0; j< instances.length; j++){
+                                                instanceIds.push(instances[j].platformId);
+                                            }
+                                        }
+                                    })
+                                }else{
+                                    return callback()
+                                }
+                                /*TaskHistory.getHistoryByTaskId(tasks[i]._id, function(err, tHistory) {
+                                    if (err) {
+                                        logger.error("Failed to fetch History for a task: ", err);
+                                        return callback(err, null);
+                                    }
+                                    if (tHistory && tHistory.length) {
+                                        tasks[i]['status'] = tHistory[0].status;
+                                        tasks[i]['startTime'] = tHistory[0].timestampStarted;
+                                        tasks[i]['endTime'] = tHistory[0].timestampEnded;
+                                    }
+                                })*/
+                            } else {
+                                return callback(null, []);
+                            }
+                        });
+                    } else {
+                        return callback(null, []);
+                    }
+                });
+            }
+        } else {
+            return callback(null, tList);
         }
     });
 };
