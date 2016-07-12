ace519448cdc6cb54c818ae36c97f7be2c1aa98a
hrushikesh07 <hrushikesh.das@relevance.com>
2016-07-12 17:02:09 +0530

    Changed the css part for composite blueprint

--- client/htmls/private/js/dev/awsProduction.js
+++ client/htmls/private/js/dev/awsProduction.js
@@ -3036,7 +3036,7 @@ function addBlueprintToDom(data) {
         }
 
         var $selectVer = $('<select></select>').addClass('blueprintVer');
-        var $liVersion = $('<li class="margin-top5">Version:&nbsp;</li>').append($selectVer);
+        var $liVersion = $('<li class="margintop10">Version:&nbsp;</li>').append($selectVer);
 
         if (data.versions) {
 
