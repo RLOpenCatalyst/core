a9bb148cb815820b1121c128ebee38706336fb8f
Divakar Konakalla <divakar.konakalla@relevancelab.com>
2016-08-05 17:27:00 +0530

    Resolved CAT-2140.

--- client/cat3/src/partials/sections/dashboard/workzone/orchestration/popups/assignNodesCtrl.js
+++ client/cat3/src/partials/sections/dashboard/workzone/orchestration/popups/assignNodesCtrl.js
@@ -8,8 +8,27 @@
 (function(angular){
 	"use strict";
 	angular.module('workzone.orchestration')
-		.controller('assignNodesCtrl', ['$scope', '$modalInstance', 'items', function($scope, $modalInstance, items) {
+		.controller('assignNodesCtrl', ['$scope', '$modalInstance', 'items', 'workzoneServices', function($scope, $modalInstance, items, workzoneServices) {
 			$scope.runlistCollection = items.taskConfig.runlist || [];
+			$scope.taskCollection = items.taskConfig.assignTasks;
+			$scope.scriptIdCollection = items.taskConfig.scriptDetails;
+			$scope.taskType = items.taskConfig.taskType;
+			$scope.scriptTypeSelelct = items.taskConfig.scriptTypeName;
+			$scope.scriptCollection = [];
+			if(items.taskConfig.taskType == 'script') {
+				workzoneServices.getScriptList($scope.scriptTypeSelelct).then(function (response) {
+					if (response.data) {
+						$scope.scriptDetails = response.data;
+						for(var i=0;i<$scope.scriptDetails.length;i++){
+							for (var j = 0; j < $scope.scriptIdCollection.length; j++) {
+								if ($scope.scriptDetails[i]._id === $scope.scriptIdCollection[j].scriptId){
+									$scope.scriptCollection.push($scope.scriptDetails[i]);
+								}
+							}
+						}
+					}
+				});
+			}
 			$scope.cancel = function() {
 				$modalInstance.dismiss('cancel');
 			};
