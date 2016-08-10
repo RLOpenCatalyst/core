d7f882811f13e02c974118db4a1cd493187845d2
Durgesh1988 <durgeshch@gmail.com>
2016-08-10 15:54:08 +0530

    CAT-2232 Fixed

--- server/app/model/unmanaged-instance/unmanaged-instance.js
+++ server/app/model/unmanaged-instance/unmanaged-instance.js
@@ -270,29 +270,20 @@ UnmanagedInstanceSchema.statics.getInstancesByProviderIdOrgIdAndPlatformId = fun
 	);
 };
 
-UnmanagedInstanceSchema.statics.updateInstanceStatus = function updateInstanceStatus(instance,callback) {
-	var updateObj = {};
-	if (instance.state === 'terminated') {
-		updateObj['state'] = instance.state;
-		updateObj['isDeleted'] = true;
-		updateObj['tags'] = instance.tags;
-		updateObj['environmentTag'] = instance.environmentTag;
-		updateObj['projectTag'] = instance.projectTag;
-	} else {
-		updateObj['state'] = instance.state;
-		updateObj['isDeleted'] = false;
-		updateObj['tags'] = instance.tags;
-		updateObj['environmentTag'] = instance.environmentTag;
-		updateObj['projectTag'] = instance.projectTag;
-	}
-	this.update({
-		"platformId": instance.platformId,
-	}, {
-		$set: updateObj
-	}, function (err, data) {
+UnmanagedInstanceSchema.statics.updateInstanceStatus = function updateInstanceStatus(instanceId,instance,callback) {
+	if(instance.state === 'terminated'){
+		instance['isDeleted'] = true;
+	}else{
+		instance['isDeleted'] = false;
+	};
+	UnmanagedInstance.update({
+			"_id": ObjectId(instanceId)
+	},{
+		$set: instance
+	}, function(err, data) {
 		if (err) {
 			logger.error("Failed to update assigned Instance status data", err);
-			callback(err, null);
+			callback(err,null);
 			return;
 		}
 		callback(null, data);
