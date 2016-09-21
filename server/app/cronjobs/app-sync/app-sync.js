7e11a24e735c3c5e744550a52fd4f287ff9bced5
Gobinda Das <gobinda.das@relevancelab.com>
2016-09-21 19:15:16 +0530

    handled memory leak issue

--- server/app/cronjobs/app-sync/app-sync.js
+++ server/app/cronjobs/app-sync/app-sync.js
@@ -7,7 +7,7 @@ var parser = require('xml2json');
 require('events').EventEmitter.defaultMaxListeners = Infinity;
 
 var AppSync = Object.create(CatalystCronJob);
-AppSync.interval = '*/2 * * * *';
+AppSync.interval = '*/1 * * * *';
 AppSync.execute = appSync;
 
 module.exports = AppSync;
@@ -21,15 +21,19 @@ function appSync() {
         if (instances && instances.length) {
             for (var i = 0; i < instances.length; i++) {
                 (function(i) {
+                    logger.debug("----------------------- ",JSON.stringify(instances[i]));
                     if (instances[i] && instances[i].instanceState === "running" && instances[i].appInfo && instances[i].appInfo.length) {
                         for (var j = 0; j < instances[i].appInfo.length; j++) {
                             (function(j) {
                                 var appInfo = instances[i].appInfo[j];
                                 if (appInfo && appInfo.appURL) {
                                     logger.debug("appInfo.appURL: ", appInfo.appURL);
-                                    client = new Client();
-                                    client.registerMethod("jsonMethod", appInfo.appURL, "GET");
-                                    var reqSubmit = client.methods.jsonMethod(function(data, response) {
+
+
+                                    var client = new Client();
+
+                                    // direct way 
+                                    var reqSubmit = client.get(appInfo.appURL, function(data, response) {
                                         var appInfoObj = {
                                             name: appInfo.name,
                                             version: appInfo.version,
@@ -44,7 +48,25 @@ function appSync() {
                                         });
                                     });
 
-                                    // Handling Exception for nexus req.
+
+                                    /*client = new Client();
+                                    client.registerMethod("jsonMethod", appInfo.appURL, "GET");
+                                    var reqSubmit = client.methods.jsonMethod(function(data, response) {
+                                        var appInfoObj = {
+                                            name: appInfo.name,
+                                            version: appInfo.version,
+                                            status: "success",
+                                            appURL: appInfo.appURL
+                                        };
+                                        instancesDao.updateAppInfo(instances[i].instanceIP, appInfoObj, function(err, data1) {
+                                            if (err) {
+                                                logger.debug("Error: ", err);
+                                            }
+                                            logger.debug("data: ", JSON.stringify(data1));
+                                        });
+                                    });*/
+
+                                    // Handling Exception for get req.
                                     reqSubmit.on('error', function(err) {
                                         logger.debug('Something went wrong on req!!', err.request.options);
 
