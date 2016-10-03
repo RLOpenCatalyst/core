571929e914ec761e513c43463beae3883936abc8
VIJAYKUMARNINGANURE <vijaykumar.ninganure@gmail.com>
2016-10-03 11:27:49 +0530

    Implemted the Docker container status

--- server/app/routes/v1.0/routes_instances.js
+++ server/app/routes/v1.0/routes_instances.js
@@ -858,7 +858,7 @@ module.exports.setRoutes = function(app, sessionVerificationFunc) {
                     projectName: instance[0].projectName,
                     envName: instance[0].environmentName,
                     envId: instance[0].envId,
-                    status: containers[0].Status,
+                    status: action.charAt(0).toUpperCase() + action.slice(1),
                     actionStatus: "pending",
                     instanceIP:instance[0].instanceIP,
                     platformId: instance[0].platformId,
