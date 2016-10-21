f529cf7a18e21d0f6155f9c0509b9dec704f782e
hrushikesh07 <hrushikesh.das@relevancelab.com>
2016-10-21 14:08:57 +0530

    Added env for launch blueprint

--- client/cat3/src/partials/sections/dashboard/workzone/blueprint/popups/blueprintLaunchParamsCtrl.js
+++ client/cat3/src/partials/sections/dashboard/workzone/blueprint/popups/blueprintLaunchParamsCtrl.js
@@ -8,26 +8,45 @@
 (function(angular){
 	"use strict";
 	angular.module('workzone.blueprint')
-		.controller('blueprintLaunchParamsCtrl', ['$scope', '$modalInstance', 'items', function($scope, $modalInstance, items) {
+		.controller('blueprintLaunchParamsCtrl', ['$scope', '$modalInstance', 'items','workzoneServices','genericServices', function($scope, $modalInstance, items,workzoneServices,genericServices) {
 			console.log(items);
 			var launchHelper = {
 				launch : function(){
-					$modalInstance.close({bp:items,stackName:$scope.stackName,domainName:$scope.domainName,tagServer:$scope.tagServer});
+					$modalInstance.close({bp:items,stackName:$scope.stackName,domainName:$scope.domainName,tagServer:$scope.tagSerSelected,launchEnv:$scope.envSeleted});
 				}
 			};
+			var bPLP=this;
+			$scope.taggingServerList=[];
+			$scope.envOptions=[]
+			workzoneServices.getTaggingServer().then(function (topSer) {
+				$scope.taggingServerList=topSer.data;
+			});
+			genericServices.getTreeNew().then(function (envData) {
+				angular.forEach(envData,function(val){
+					if(val.rowid === items.orgId){
+						$scope.orgSeleted=val.name;
+						angular.forEach(val.businessGroups,function(busval){
+							if(busval.rowid === items.bgId){
+								$scope.busSeleted=busval.name;
+								angular.forEach(busval.projects,function(projval){
+									if(projval.rowId === items.projectId){
+										$scope.projSeleted=projval.name;
+										$scope.envOptions=projval.environments;
+										$scope.envSeleted=projval.environments[0].rowid;
+									}
+								});
+							}
+						});
+					}
+
+				});
+
+			});
 			$scope.stackName='';
 			$scope.domainName='';
-			$scope.tagServer = "Monitoring";
+			$scope.tagSerSelected = "";
 			$scope.cancel = function() {
 				$modalInstance.dismiss('cancel');
-			};
-			$scope.tagServerChecking = function() {
-				if($scope.tagServerCheck){
-					$scope.tagServerStatus = true;
-				}else{
-					$scope.tagServerStatus = false;
-					$scope.tagServer = '';
-				}
 			};
 			$scope.launchBP = function() {
 				if(items.blueprintType === "aws_cf") {
