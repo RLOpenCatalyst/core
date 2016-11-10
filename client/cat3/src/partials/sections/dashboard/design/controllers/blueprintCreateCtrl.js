2e48612151bf385afc6f2d7a9f8f23d326e3505d
srikanthv02 <srikanthv02@gmail.com>
2016-11-10 21:01:33 +0530

    Added check for service delivery in design

--- client/cat3/src/partials/sections/dashboard/design/controllers/blueprintCreateCtrl.js
+++ client/cat3/src/partials/sections/dashboard/design/controllers/blueprintCreateCtrl.js
@@ -341,6 +341,16 @@
                 }
             };
 
+            blueprintCreation.botTypes = function() {
+                if(blueprintCreation.newEnt.serviceDelivery_isChecked) {
+                    $scope.botTypes = true;
+                    blueprintCreation.newEnt.botTypeValue = "Task";
+                    blueprintCreation.newEnt.botCategoryValue  = "Active Directory";
+                } else {
+                    $scope.botTypes = false;
+                }
+            };
+
             blueprintCreation.showRepoServers = function() {
                 if(blueprintCreation.newEnt.appDeployCheck_isChecked) {
                     bpCreateSer.getNexusServerList().then(function(data){
@@ -732,7 +742,12 @@
                             console.log(blueprintCreateJSON.dockercompose);
                         });
                     }
-
+                    if(blueprintCreation.newEnt.serviceDelivery_isChecked){
+                        blueprintCreateJSON.botType = blueprintCreation.newEnt.botTypeValue;
+                        blueprintCreateJSON.shortDesc = blueprintCreation.newEnt.botDescription;
+                        blueprintCreateJSON.botCategory = blueprintCreation.newEnt.botCategoryValue;
+                        blueprintCreateJSON.serviceDeliveryCheck = true;
+                    }
                     if(blueprintCreation.newEnt.appDeployCheck_isChecked){
                         if(blueprintCreation.newEnt.serverType === 'nexus'){
                             var nexus = {
