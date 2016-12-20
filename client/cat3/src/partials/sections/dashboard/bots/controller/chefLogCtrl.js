c6f7c876e78f67bbe4a1c4239a3f2fdaa6542627
Durgesh1988 <durgeshch@gmail.com>
2016-12-20 18:58:26 +0530

    Resolve UI error for PROD Build

--- client/cat3/src/partials/sections/dashboard/bots/controller/chefLogCtrl.js
+++ client/cat3/src/partials/sections/dashboard/bots/controller/chefLogCtrl.js
@@ -8,7 +8,7 @@
 (function (angular) {
 	"use strict";
 	angular.module('dashboard.bots')
-		.controller('chefLogCtrl', ['$q', '$scope', 'workzoneServices', '$timeout', 'orchestrationSetting', 'toastr', function ($q, $scope, workzoneServices, $timeout, orchestrationSetting, toastr) {
+		.controller('chefLogCtrl', ['$q', '$scope', 'workzoneServices', '$timeout', 'orchestrationSetting', 'toastr','items', function ($q, $scope, workzoneServices, $timeout, orchestrationSetting, toastr,items) {
 			/*This controller can be invoked from either of two flows - Chef Log History Item Logs OR Chef Task Execute
 			 items object will contain only taskId and historyId. */
 			//var items = $scope.parentItemDetail;
@@ -44,21 +44,21 @@
 					}, 100);
 				},
 				/*logsPolling: function () {
-					timerObject = $timeout(function () {
-						workzoneServices.getChefJobLogs($scope.selectedInstance.nodeId, $scope.selectedInstance.actionLogId, helper.lastTimeStamp).then(function (resp) {
-							if (resp.data.length) {
-								var logData = {
-									logs: resp.data,
-									fullLogs: false
-								};
-								helper.lastTimeStamp = helper.getlastTimeStamp(logData.logs);
-								$scope.logListDelta.push.apply($scope.logListDelta, logData.logs);
-								helper.scrollBottom();
-							}
-							helper.logsPolling();
-						});
-					}, orchestrationSetting.orchestrationLogsPollerInterval);
-				},*/
+				 timerObject = $timeout(function () {
+				 workzoneServices.getChefJobLogs($scope.selectedInstance.nodeId, $scope.selectedInstance.actionLogId, helper.lastTimeStamp).then(function (resp) {
+				 if (resp.data.length) {
+				 var logData = {
+				 logs: resp.data,
+				 fullLogs: false
+				 };
+				 helper.lastTimeStamp = helper.getlastTimeStamp(logData.logs);
+				 $scope.logListDelta.push.apply($scope.logListDelta, logData.logs);
+				 helper.scrollBottom();
+				 }
+				 helper.logsPolling();
+				 });
+				 }, orchestrationSetting.orchestrationLogsPollerInterval);
+				 },*/
 				stopPolling: function () {
 					$timeout.cancel(timerObject);
 				}
@@ -76,18 +76,18 @@
 			};
 			var init = function () {
 				//get the details of one chef history entry
-				var param = { 
-					url = '/bots/' + botId + '/history/' + historyId;
+				var param = {
+					url : '/bots/' + botId + '/history/' + historyId
 				}
 				genSevs.promiseGet(param).then(function (response) {
-	                chefLogData.createInstanceList(response.data);
-	                //$modalInstance.close(response.data);
-	                //$rootScope.$emit('BOTS_LIBRARY_REFRESH');
-	                //helper.botLogModal(items._id, response.historyId, response.taskType);
-	            };
-				/*workzoneServices.getTaskHistoryItem(items.taskId, items.historyId).then(function (response) {
 					chefLogData.createInstanceList(response.data);
-				});*/
+					//$modalInstance.close(response.data);
+					//$rootScope.$emit('BOTS_LIBRARY_REFRESH');
+					//helper.botLogModal(items._id, response.historyId, response.taskType);
+				});
+				/*workzoneServices.getTaskHistoryItem(items.taskId, items.historyId).then(function (response) {
+				 chefLogData.createInstanceList(response.data);
+				 });*/
 			};
 			//method to get the names of the nodes associated to the job and add it to the nodeIdsWithActionLog object.
 			chefLogData.createInstanceList = function (historyItem) {
@@ -109,40 +109,40 @@
 					"instanceIds": nodeIds
 				};
 				workzoneServices.postRetrieveDetailsForInstanceNames(requestObj).then(function (response) {
-					var _jobInstances = response.data;
-					//if blueprint job, use blueprintExecutionResults
-					if (bluePrintJob) {
-						for (var i = 0; i < bluePrintActionLogs.length; i++) {
-							bluePrintActionLogs[i].nodeId = bluePrintActionLogs[i].instanceId;
-							for (var j = 0; j < _jobInstances.length; j++) {
-								if (bluePrintActionLogs[i].instanceId === _jobInstances[j]._id) {
-									bluePrintActionLogs[i].uiNodeName = _jobInstances[j].name;
+						var _jobInstances = response.data;
+						//if blueprint job, use blueprintExecutionResults
+						if (bluePrintJob) {
+							for (var i = 0; i < bluePrintActionLogs.length; i++) {
+								bluePrintActionLogs[i].nodeId = bluePrintActionLogs[i].instanceId;
+								for (var j = 0; j < _jobInstances.length; j++) {
+									if (bluePrintActionLogs[i].instanceId === _jobInstances[j]._id) {
+										bluePrintActionLogs[i].uiNodeName = _jobInstances[j].name;
+									}
 								}
 							}
-						}
-						nodeIdWithActionLogs = bluePrintActionLogs;
-					} else {//if not blueprint job, use nodeIdsWithActionLog
-						for (var k = 0; k < historyItem.nodeIdsWithActionLog.length; k++) {
-							for (var l = 0; l < _jobInstances.length; l++) {
-								if (historyItem.nodeIdsWithActionLog[k].nodeId === _jobInstances[l]._id) {
-									historyItem.nodeIdsWithActionLog[k].uiNodeName = _jobInstances[l].name;
+							nodeIdWithActionLogs = bluePrintActionLogs;
+						} else {//if not blueprint job, use nodeIdsWithActionLog
+							for (var k = 0; k < historyItem.nodeIdsWithActionLog.length; k++) {
+								for (var l = 0; l < _jobInstances.length; l++) {
+									if (historyItem.nodeIdsWithActionLog[k].nodeId === _jobInstances[l]._id) {
+										historyItem.nodeIdsWithActionLog[k].uiNodeName = _jobInstances[l].name;
+									}
 								}
 							}
+							nodeIdWithActionLogs = historyItem.nodeIdsWithActionLog;
 						}
-						nodeIdWithActionLogs = historyItem.nodeIdsWithActionLog;
-					}
-					chefLogData.chefHistoryItem = historyItem; //saved as we need timestamps from the historyItem
-					chefLogData.nodeIdsWithActionLog = nodeIdWithActionLogs; //this can now be used to show instance dropdown
-					if (chefLogData.nodeIdsWithActionLog[0]) {
+						chefLogData.chefHistoryItem = historyItem; //saved as we need timestamps from the historyItem
+						chefLogData.nodeIdsWithActionLog = nodeIdWithActionLogs; //this can now be used to show instance dropdown
+						if (chefLogData.nodeIdsWithActionLog[0]) {
+							$scope.isInstanceListLoading = false;
+							selectFirstInstance(chefLogData.nodeIdsWithActionLog[0]);
+						}
+					},
+					function (error) {
 						$scope.isInstanceListLoading = false;
-						selectFirstInstance(chefLogData.nodeIdsWithActionLog[0]);
-					}
-				},
-				function (error) {
-					$scope.isInstanceListLoading = false;
-					toastr.error(error);
-					$scope.errorMessage = "";
-				});
+						toastr.error(error);
+						$scope.errorMessage = "";
+					})
 			};
 			//method called when user changes the instance name from the dropdown.
 			chefLogData.instanceChange = function () {
@@ -161,32 +161,32 @@
 					if(timestamp && timestampEnded) {
 						urlParams = 'timestamp=' + timestamp  + '&timestampEnded=' + timestampEnded;
 					}
-					var param = { 
-						url = "/instances/" + instanceId + '/actionLogs/' + actionId +
-							'/logs?' + urlParams;
+					var param = {
+						url : "/instances/" + instanceId + '/actionLogs/' + actionId +
+						'/logs?' + urlParams
 					}
 					genSevs.promiseGet(param).then(function (response) {
-		                $scope.isLogsLoading = false;
+						$scope.isLogsLoading = false;
 						var logData = {
 							logs: response.data,
 							fullLogs: true
 						};
 						$scope.logListInitial = logData.logs;
 						helper.scrollBottom();
-		            };
+					});
 					/*workzoneServices.getChefJobLogs($scope.selectedInstance.nodeId, $scope.selectedInstance.actionLogId, chefLogData.chefHistoryItem.timestampStarted, chefLogData.chefHistoryItem.timestampEnded).then(function (response) {
-						$scope.isLogsLoading = false;
-						var logData = {
-							logs: response.data,
-							fullLogs: true
-						};
-						$scope.logListInitial = logData.logs;
-						helper.scrollBottom();
-					}, function (error) {
-						$scope.isLogsLoading = false;
-						console.log(error);
-						$scope.errorMessage = "Unable to fetch logs for this instance";
-					});*/
+					 $scope.isLogsLoading = false;
+					 var logData = {
+					 logs: response.data,
+					 fullLogs: true
+					 };
+					 $scope.logListInitial = logData.logs;
+					 helper.scrollBottom();
+					 }, function (error) {
+					 $scope.isLogsLoading = false;
+					 console.log(error);
+					 $scope.errorMessage = "Unable to fetch logs for this instance";
+					 });*/
 				}
 				/* when task is pending or running: ie. during Execute flow or if history is viewed immediately after Execute
 				 - only timestampStarted will be present. timestampEnded will be absent
@@ -200,34 +200,34 @@
 					if(timestamp && timestampEnded) {
 						urlParams = 'timestamp=' + timestamp  + '&timestampEnded=' + timestampEnded;
 					}
-					var param = { 
-						url = "/instances/" + instanceId + '/actionLogs/' + actionId +
-							'/logs?' + urlParams;
+					var param = {
+						url : "/instances/" + instanceId + '/actionLogs/' + actionId +
+						'/logs?' + urlParams
 					}
 					genSevs.promiseGet(param).then(function (response) {
-		                $scope.isLogsLoading = false;
+						$scope.isLogsLoading = false;
 						var logData = {
 							logs: response.data,
 							fullLogs: true
 						};
 						$scope.logListInitial = logData.logs;
 						helper.scrollBottom();
-		            };
+					});
 					/*workzoneServices.getChefJobLogs($scope.selectedInstance.nodeId, $scope.selectedInstance.actionLogId, chefLogData.chefHistoryItem.timestampStarted).then(function (response) {
-						$scope.isLogsLoading = false;
-						helper.lastTimeStamp = helper.getlastTimeStamp(response.data) || chefLogData.chefHistoryItem.timestampStarted;
-						helper.logsPolling();
-						var logData = {
-							logs: response.data,
-							fullLogs: true
-						};
-						$scope.logListInitial = logData.logs;
-						helper.scrollBottom();
-					}, function (error) {
-						$scope.isLogsLoading = false;
-						console.log(error);
-						$scope.errorMessage = "Unable to fetch logs for this instance";
-					});*/
+					 $scope.isLogsLoading = false;
+					 helper.lastTimeStamp = helper.getlastTimeStamp(response.data) || chefLogData.chefHistoryItem.timestampStarted;
+					 helper.logsPolling();
+					 var logData = {
+					 logs: response.data,
+					 fullLogs: true
+					 };
+					 $scope.logListInitial = logData.logs;
+					 helper.scrollBottom();
+					 }, function (error) {
+					 $scope.isLogsLoading = false;
+					 console.log(error);
+					 $scope.errorMessage = "Unable to fetch logs for this instance";
+					 });*/
 				}
 			};
 			init();
@@ -239,5 +239,5 @@
 			});
 			return chefLogData;
 		}
-	]);
+		]);
 })(angular);
