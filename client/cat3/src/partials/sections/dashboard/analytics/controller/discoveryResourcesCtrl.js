0b7fd437864f807ee433ff792c9e177ea7296a7b
hrushikesh07 <hrushikesh.das@relevancelab.com>
2016-12-26 14:00:01 +0530

    update tag mapping

--- client/cat3/src/partials/sections/dashboard/analytics/controller/discoveryResourcesCtrl.js
+++ client/cat3/src/partials/sections/dashboard/analytics/controller/discoveryResourcesCtrl.js
@@ -3,48 +3,77 @@
     angular.module('dashboard.analytics')
         .controller('discoveryResourcesCtrl', ['$scope', '$rootScope', '$state','analyticsServices', 'genericServices','$timeout', function ($scope,$rootScope,$state,analyticsServices,genSevs,$timeout){
             var disResrc=this;
-            $scope.TangName={};
-            $scope.tagValue=[{'id':'a','value':'a'},{'id':'b','value':'b'}]
-
+            $scope.TangName={
+                environment:[],
+                bg:[],
+                project:[]
+            };
             // get gat name  Start
             disResrc.getAllTagNames=function () {
-                    // environment
-                    var param = {
-                        url: '/providers/' + fltrObj.provider.id + '/tag-mappings/environment'
-                    };
-                    genSevs.promiseGet(param).then(function (instResult) {
-                        $scope.TangName.environment = instResult.tagValues;
+                // environment
+                var param = {
+                    url: '/providers/' + fltrObj.provider.id + '/tag-mappings/environment'
+                };
+                genSevs.promiseGet(param).then(function (instResult) {
+                    angular.forEach(instResult.tagValues,function(val){
+                        $scope.TangName.environment.push({id:val,name:val})
                     });
-                    // Bu
-                    var param = {
-                        url: '/providers/' + fltrObj.provider.id + '/tag-mappings/bgName'
-                    };
-                    genSevs.promiseGet(param).then(function (instResult) {
-                        $scope.TangName.bg = instResult.tagValues;
+                });
+                // Bu
+                var param = {
+                    url: '/providers/' + fltrObj.provider.id + '/tag-mappings/bgName'
+                };
+                genSevs.promiseGet(param).then(function (instResult) {
+                    angular.forEach(instResult.tagValues,function(val){
+                        $scope.TangName.bg.push({id:val,name:val})
                     });
-                    // project
-                    var param = {
-                        url: '/providers/' + fltrObj.provider.id + '/tag-mappings/project'
-                    };
-                    genSevs.promiseGet(param).then(function (instResult) {
-                        $scope.TangName.project = instResult.tagValues;
+                });
+                // project
+                var param = {
+                    url: '/providers/' + fltrObj.provider.id + '/tag-mappings/project'
+                };
+                genSevs.promiseGet(param).then(function (instResult) {
+                    angular.forEach(instResult.tagValues,function(val){
+                        $scope.TangName.project.push({id:val,name:val})
                     });
-                    console.log($scope.TangName);
+
+                });
+
                 // get gat name  End##########
             };
-            disResrc.gridOptionInstances = {
-                columnDefs : [
-                { name: 'InstanceId',field:'platformId' },
-                { name: 'os', displayName:'OS', enableCellEdit: true, type: 'number'},
-                { name: 'privateIpAddress', displayName: 'IP Address'},
-                { name: 'state', displayName: 'Status'},
-                { name: 'Region', displayName: 'Region',field:'providerData.region_name',cellTooltip: true},
-                { name: 'Bg Tag Value',width:300, enableCellEdit: true,editableCellTemplate: 'ui-grid/dropdownEditor',
-                    editDropdownOptionsArray: $scope.tagValue},
-                { name: 'Project Tag Value', enableCellEdit: true},
-                { name: 'Env. Tag Value', enableCellEdit: true}
-            ]
+            disResrc.init=function () {
+                disResrc.getAllTagNames();
+                $timeout(function () {
+                disResrc.gridOptionInstances = {
+                    columnDefs : [
+                        { name: 'InstanceId',field:'platformId' },
+                        { name: 'os', displayName:'OS', enableCellEdit: true, type: 'number'},
+                        { name: 'privateIpAddress', displayName: 'IP Address'},
+                        { name: 'state', displayName: 'Status'},
+                        { name: 'Region', displayName: 'Region',field:'providerData.region_name',cellTooltip: true},
+                        { name: 'Bg Tag Value',width:300, enableCellEdit: true,editableCellTemplate: 'ui-grid/dropdownEditor',
+                            editDropdownOptionsArray:$scope.TangName.bg, editDropdownIdLabel: 'name',
+                            editDropdownValueLabel: 'id'},
+                        { name: 'Project Tag Value', enableCellEdit: true,editableCellTemplate: 'ui-grid/dropdownEditor',
+                            editDropdownOptionsArray:$scope.TangName.project, editDropdownIdLabel: 'name',
+                            editDropdownValueLabel: 'id'},
+                        { name: 'Env. Tag Value', enableCellEdit: true,editableCellTemplate: 'ui-grid/dropdownEditor',
+                            editDropdownOptionsArray:$scope.TangName.environment, editDropdownIdLabel: 'name',
+                            editDropdownValueLabel: 'id'}
+                    ]
+                };
+                    disResrc.gridOptionInstances.data=[];
+                    if(fltrObj && fltrObj.provider && fltrObj.provider.id) {
+                        var param = {
+                            url: '/providers/' + fltrObj.provider.id +'/'+ $rootScope.organNewEnt.instanceType
+                        };
+                        genSevs.promiseGet(param).then(function (instResult) {
+                            disResrc.gridOptionInstances.data=instResult.data;
+                        });
+                    }
+                },200);
             };
+
             $rootScope.stateItems = $state.params;
             $rootScope.organNewEnt.provider='0';
             $rootScope.organNewEnt.instanceType='unassigned-instances';
@@ -52,24 +81,11 @@
             var treeNames = ['Cloud Management','Discovery','Resources'];
             $rootScope.$emit('treeNameUpdate', treeNames);
             var fltrObj=$rootScope.filterNewEnt;
-            disResrc.getInstances =function () {
-                disResrc.gridOptionInstances.data=[];
-                if(fltrObj && fltrObj.provider && fltrObj.provider.id) {
-                    var param = {
-                        url: '/providers/' + fltrObj.provider.id +'/'+ $rootScope.organNewEnt.instanceType
-                    };
-                    disResrc.getAllTagNames();
-                    genSevs.promiseGet(param).then(function (instResult) {
-                        disResrc.gridOptionInstances.data=instResult.data;
-                    });
-                }
-            };
-
             $rootScope.applyFilter =function(filterApp,period){
                 analyticsServices.applyFilter(true,null);
-                disResrc.getInstances();
+                disResrc.init();
             };
-            disResrc.getInstances();
+            disResrc.init();
             
         }]);
 })(angular);
