61af360cb0c5becfe7466c43155c6fdeaed4725a
hrushikesh07 <hrushikesh.das@relevancelab.com>
2017-01-06 16:45:19 +0530

    new capacity

--- client/cat3/src/partials/sections/dashboard/analytics/controller/capacityCtrl.js
+++ client/cat3/src/partials/sections/dashboard/analytics/controller/capacityCtrl.js
@@ -1,9 +1,18 @@
 (function (angular) {
     "use strict";
     angular.module('dashboard.analytics')
-        .controller('capacityCtrl', ['$scope', '$rootScope', '$state','analyticsServices', 'genericServices','$timeout', function ($scope,$rootScope,$state,analyticsServices,genSevs,$timeout){
-            $rootScope.stateItems = $state.params;
+        .controller('capacityCtrl', ['$scope', '$rootScope', '$state','analyticsServices', 'genericServices','$timeout','$location', '$anchorScroll','$modal',function ($scope,$rootScope,$state,analyticsServices,genSevs,$timeout,$location, $anchorScroll,$modal){
+
+            $rootScope.organNewEnt.provider='0';
             //analyticsServices.initFilter();
+            $scope.gototab = function(id) {
+                // set the location.hash to the id of
+                // the element you wish to scroll to.
+                $location.hash(id);
+
+                // call $anchorScroll()
+                $anchorScroll();
+            };
             var capaCtr =this;
             capaCtr.chartData=[];
             capaCtr.splitUp=null;
@@ -167,6 +176,7 @@
                     }
                     if(result.capacity && result.capacity.AWS && result.capacity.AWS.services) {
                         capaCtr.serviceCapacity = result.capacity.AWS.services;
+                        capaCtr.createList();
                         angular.forEach(result.capacity.AWS.services, function (valueChild, keyChild) {
                             var va = [];
                             capaCtr.capaGridOptions.columnDefs.push({
@@ -321,7 +331,156 @@
                     $rootScope.$emit('treeNameUpdate', treeNames);
                 },500);
             };
+            // new e###################################
+            $scope.openChart=function (id) {
+                $modal.open({
+                    animation: true,
+                    templateUrl: 'src/partials/sections/dashboard/analytics/view/capacityChart.html',
+                    controller: 'capacityChartCtrl as capChat',
+                    backdrop : 'static',
+                    keyboard: false,
+                    resolve: {
+                        items: function() {
+                            return id;
+                        }
+                    }
+                }).
+                result.then(function() {
+
+                }, function() {
+
+                });
+            };
+            $scope.chefConfig=function (id) {
+                var promise = genSevs.editRunlist(id);
+            };
+            capaCtr.createList = function () {
+                capaCtr.listGrid=[];
+                var fltrObj=$rootScope.filterNewEnt;
+
+               // for(var value in capaCtr.serviceCapacity) {
+                var value='RDS';
+                    capaCtr.listGrid[value]=[];
+                    capaCtr.listGrid[value].data=[];
+                    capaCtr.listGrid[value].paginationPageSizes= [25, 50, 100];
+                    capaCtr.listGrid[value].paginationPageSize=25;
+                    capaCtr.listGrid[value].columnDefs=[
+                        {name: 'platformId', field: 'platformId', cellTooltip: true},
+                        {name: 'os', enableFiltering: true,displayName: 'os', field:'os',cellTooltip: true},
+                        {name: 'privateIpAddress', displayName: 'IP Address',cellTooltip: true},
+                        {name: 'state', displayName: 'Status',cellTooltip: true},
+                        {name: 'Region',displayName: 'Region',
+                            field: 'providerData.region',
+                            cellTooltip: true
+                        },
+                        {name: 'cost', displayName: 'cost',cellTemplate: '<span>12</span>'},
+                        {name: 'Usage', cellTooltip: true,cellTemplate:"<span class='cursor' ng-click='grid.appScope.openChart(row.entity)'><i class=\"fa fa-bar-chart\"></i></span>"},
+                        {name: 'Chef', cellTooltip: true,cellTemplate:"<span class='cursor' ng-click='grid.appScope.chefConfig(row.entity)'><i  class=\"fa fa-eye\" title=\"Chef Configuration\"></i></span>"}
+                    ];
+                if(fltrObj && fltrObj.provider && fltrObj.provider.id) {
+                    var param = {
+                        url: '/providers/' + fltrObj.provider.id + '/unassigned-instances'
+                        // url:'src/partials/sections/dashboard/analytics/data/ins.json'
+                    };
+                    genSevs.promiseGet(param).then(function (instResult) {
+                        capaCtr.listGrid[value].data=instResult.data;
+                    });
+                }
+
+                //}
+            };
+
             capaCtr.init();
 
-        }]);
+
+        }]).controller('capacityChartCtrl',['$scope','$rootScope','items','genericServices','$modalInstance',function($scope,$rootScope,items,genSevs,$modalInstance){
+            var capChat=this;
+            capChat.items=items;
+        capChat.trendLineChart={
+            options:{},
+            data:[]
+        };
+        capChat.splitUp='CPUUtilization';
+            capChat.trendLineChart.options = {
+                chart: {
+                    //type: 'stackedAreaChart',
+                    type: 'lineChart',
+                    height: 200,
+                    margin: {
+                        top: 20,
+                        right: 20,
+                        bottom:70,
+                        left: 40
+                    },
+                    x: function (d) {
+                        return d[0];
+                    },
+                    y: function (d) {
+                        return d[1];
+                    },
+                    useVoronoi: false,
+                    clipEdge: true,
+                    duration: 10,
+                    useInteractiveGuideline: true,
+                    xAxis: {
+                        axisLabel: 'Date',
+                        showMaxMin: false,
+                        tickFormat: function (d) {
+                            return d3.time.format('%d/%m %H:%M')(new Date(d));
+                        }
+                    },
+                    yAxis: {
+                        tickFormat: function (d) {
+                            return d3.format(',.2f')(d);
+                        }
+                    },
+                    zoom: {
+                        enabled: true,
+                        scaleExtent: [1, 10],
+                        useFixedDomain: false,
+                        useNiceScale: false,
+                        horizontalOff: true,
+                        verticalOff: true,
+                        unzoomEventType: 'dblclick.zoom'
+                    }
+                }
+            };
+            capChat.legends=[];
+        
+        capChat.getData=function(fltObj){
+            capChat.trendLineChart.data = [];
+            var  $today = new Date();
+            var $yesterday = new Date($today);
+            $yesterday.setDate($today.getDate() - 1);
+            if(fltObj && fltObj.resources && fltObj.resources.length >0) {
+                var  $today = new Date();
+                var $yesterday = new Date($today);
+                $yesterday.setDate($today.getDate() - 1);
+                    var param = {
+                        url: '/analytics/trend/usage?resource=' + items._id + '&fromTimeStamp=' + $yesterday + '&toTimeStamp=' + $today + '&interval=3600'
+                        //url:'src/partials/sections/dashboard/analytics/data/usage.json'
+                    };
+                    genSevs.promiseGet(param).then(function (result) {
+                        var va = [];
+                        if (result) {
+                            angular.forEach(result[capChat.splitUp].dataPoints, function (value) {
+                                va.push([Date.parse(value.fromTime), value.average]);
+                            });
+                            capChat.trendLineChart.data.push({
+                                "key": items.platformId,
+                                "values": va
+                            });
+                        }
+                        // }
+                    });
+            }
+        };
+        capChat.splitChange=function() {
+            capChat.getData($rootScope.filterNewEnt);
+        };
+        capChat.getData($rootScope.filterNewEnt);
+        $scope.cancel = function() {
+            $modalInstance.dismiss('cancel');
+        };
+    }]);
 })(angular);
