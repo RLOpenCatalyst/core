8e729c19caead59e345954999a247752b6d7f6b4
vandanapainuly <painuly.bandu@gmail.com>
2017-04-07 11:41:44 +0530

    changed Ui for service Now and Service Status

--- client/cat3/service_dashboard/js/serviceNow.js
+++ client/cat3/service_dashboard/js/serviceNow.js
@@ -26,99 +26,41 @@ if (!$.fn.dataTable.isDataTable('#snTable')) {
 var stateCall=null;
 var statePrd= '';
 $(document).ready(function(){
-    $.get('http://13.82.225.212:8080/ticket-analyser/getsummary', function(data){
-         $('#total').html(data.summary.total);
-         $('#open').html(data.summary.open);
-         $('#closed').html(data.summary.closed);
-         $('#missedSla').html(data.summary.missed_sla);
-         $('#about_to_miss_sla').html(data.summary.about_to_miss_sla);
-         $('#unassigned').html(data.summary.unassigned);
-      });
-
     stateCall= 'openTickets';
-    
-    periodFilter('getdailyincidents'); 
-   
-    
-    // tickets(status);
-    /*slaMissed()*/
-    // serviceNowDetails('/public/serviceNow.json');
-    // tickets();
-    // $("#weekly").click(function(){
-    //     alert("weekly");
-    //     serviceNowDetails('http://192.168.152.86:9090/ticket-analyser/getweeklysummary');
-    // });
-    // $("#monthly").click(function(){
-    //     alert("monthly");
-    //     serviceNowDetails('http://192.168.152.86:9090/ticket-analyser/getweeklysummary');
-    // });
-    // $("#daily").click(function(){
-    //     alert("monthly");
-    //     serviceNowDetails('http://192.168.152.86:9090/ticket-analyser/getdailysummary');
-    // });
-    //dailySN("http://192.168.152.86:9090/ticket-analyser/getdailysummary");
+    periodFilter('getdailysummary'); 
 });
-    //jQuery.support.cors = true;
-// function serviceNowDetails() {
-    
-//     $.ajax({
-//         type: "GET",
-//         url: service + '/serviceNow.json',
-//         contentType: "application/json",
-//         dataType: "json",
-//         cache: false,
-//         success: function (data) {
-//             alert(data);
-//             if(data && data.incidents && data.incidents.length){
-//                 var trHTML = '';
-//               for(i=0; i<data.incidents.length; i++) { 
-//                 trHTML += '<tr><td>' + data.incidents[i].number + '</td>'
-//                              '<td>' + data.incidents[i].createdDate + '</td>'
-//                              '<td>' + data.incidents[i].assigned_to + '</td>'
-//                              '<td>' + data.incidents[i].category + '</td>'
-//                              '<td>' + data.incidents[i].priority + '</td>'
-//                              '<td>' + data.incidents[i].shortDesc + '<td>'
-//                              '<td>' + data.incidents[i].workLog + '</td></tr>';
-//               }
-//               $('#snTable').html(trHTML);
-//             }
-//         }
-//     });
-// }
 
+function periodFilter(prd) {
+    $('.pridAct').removeClass('active');
+    $('#'+prd).last().addClass("active");
+    statePrd=prd;
+    $.get('http://13.82.225.212:8080/ticket-analyser/'+prd, function(data){
+        $('#total').html(data.summary.total);
+        $('#open').html(data.summary.open);
+        $('#closed').html(data.summary.closed);
+        $('#missedSla').html(data.summary.missed_sla);
+        $('#about_to_miss_sla').html(data.summary.about_to_miss_sla);
+        $('#unassigned').html(data.summary.unassigned);
+        switch(prd){
+        case 'getdailysummary' : 
+         statePrd='getdailyincidents';
+        ticketsList(stateCall);
+        break;
 
+        case 'getweeklysummary' : 
+         statePrd='getweeklyincidents';
+        ticketsList(stateCall);
+        break;
 
-/*function slaMissed() {
-    $.get('http://localhost:8080/ticket-analyser/getweeklysummary?state=closed&page=1&size=20', function(data){
-        //var width=(1/4*100);
-        var total_missed_sla = data.summary.p1_sla_missed + data.summary.p2_sla_missed + data.summary.p3_sla_missed + data.summary.p4_sla_missed;
-        var p1 = ((data.summary.p1_sla_missed/total_missed_sla) *100);
-        $('#p1').html(p1.toFixed(2)+"%");
-        var p2 = ((data.summary.p2_sla_missed/total_missed_sla) *100);
-        $('#p2').html(p2.toFixed(2)+ "%");
-        var p3 = ((data.summary.p3_sla_missed/total_missed_sla) *100);
-        $('#p3').html(p3.toFixed(2)+ "%");
-        var p4 = ((data.summary.p4_sla_missed/total_missed_sla) *100);
-        $('#p4').html(p4.toFixed(2)+ "%");
-
+        case 'getmonthlysummary' :
+         statePrd='getmonthlyincidents'; 
+        ticketsList(stateCall);
+        break;
+        }
     });
-}*/
+};
 
-
-
-// function serviceNowDetails(url) {
-//     $.ajax({
-//         url:url,
-//         type: 'GET',
-//         dataType: 'jsonp',
-//         success:function(content,code)
-//         {
-//             console.log(JSON.parse(code));
-//         }      
-//     });
-// };
-
-function tickets(state) { 
+function ticketsList(state) { 
     stateCall= state;
     prd=statePrd;
     $('.act').removeClass('selected-section');
@@ -170,32 +112,30 @@ function tickets(state) {
     }
 }
 
-function periodFilter(prd) {
-    $('.pridAct').removeClass('active');
-    $('#'+prd).last().addClass("active");
-    statePrd=prd;
-    tickets(stateCall);
-};
-
 function tableCreate(data) {
+       
     var total_missed_sla = data.summary.p1 + data.summary.p2 + data.summary.p3 + data.summary.p4;
     var p1 = ((data.summary.p1/total_missed_sla) *100);
+    p1 = p1 || 0;
     $('#p1').html(p1.toFixed(0)+"%");
     console.log(p1);
     //$('#p1').css('width',p1.toFixed(2)+"%");
     var p2 = ((data.summary.p2/total_missed_sla) *100);
+    p2 = p2 || 0;
     $('#p2').html(p2.toFixed(0)+ "%");
+    console.log(p2);
     //$('#p2').css('width',p2.toFixed(2)+ "%");
     var p3 = ((data.summary.p3/total_missed_sla) *100);
+    p3 = p3 || 0;
     $('#p3').html(p3.toFixed(0)+ "%");
+    console.log(p3);
     //$('#p3').css('width',p3.toFixed(2)+ "%");
     var p4 = ((data.summary.p4/total_missed_sla) *100);
+    p4 = p4 || 0;
     $('#p4').html(p4.toFixed(0)+ "%");
     console.log(p4);
     //$('#p4').css('width',p4.toFixed(2)+ "%");
      
-    /*var responseAdd=((+data.summary.response) + (+data.summary.p2_sla_missed_response) + (+data.summary.p3_sla_missed_response) + (+data.summary.p4_sla_missed_response)); 
-    var responseAvg=responseAdd/4;*/
     if(data && data.summary && data.summary.response){
         var resPer=(data.summary.response/data.summary.total)*100;
         $('#response').html(resPer.toFixed(0)+"%");
@@ -207,8 +147,6 @@ function tableCreate(data) {
         $('#response').css('color',"#000");
     }
 
-    /*var resolutionAdd = data.summary.p1_sla_missed_resolution + data.summary.p2_sla_missed_resolution + data.summary.p3_sla_missed_resolution + data.summary.p4_sla_missed_resolution;
-    var resolutionAvg = resolutionAdd/4;*/
     if(data && data.summary && data.summary.resolution){
         var resolutionPer = (data.summary.resolution/data.summary.total)*100;
         $('#resolution').html(resolutionPer.toFixed(0)+"%");
@@ -219,6 +157,22 @@ function tableCreate(data) {
         $('#resolution').css('width',"0%");
         $('#resolution').css('color',"#000");
     }
+
+    $('#total').html(data.summary.total);
+    $('#open').html(data.summary.open);
+    $('#closed').html(data.summary.closed);
+    $('#missedSla').html(data.summary.missed_sla);
+    $('#about_to_miss_sla').html(data.summary.about_to_miss_sla);
+    $('#unassigned').html(data.summary.unassigned);
+
+    $('#marq').empty().append("Critical Priority Incidents: ");
+    for(i=0; i<data.summary.incidents.length; i++) {
+        /*var mar= ;*/
+        if(data.summary.incidents[i].priority === "1 - Critical") {
+            $('#marq').append(" <span style='color:red'>" +data.summary.incidents[i].number+'</span> | ');
+        }
+    }
+
     /*var ATMSresolutionAdd = data.summary.p1_about_to_miss_sla_resolution + data.summary.p2_about_to_miss_sla_resolution + data.summary.p3_about_to_miss_sla_resolution + data.summary.p4_about_to_miss_sla_resolution;
     var ATMSresolutionAvg = ATMSresolutionAdd/4;
     var ATMSresolutionPer = (ATMSresolutionAvg/ATMSresolutionAdd)*100;
@@ -234,26 +188,22 @@ function tableCreate(data) {
 
 
     $('#snTable').DataTable( {
-            // "processing": true,
-            "processing": true,
-                'language':{ 
-                   "loadingRecords": "&nbsp;",
-                   "processing": "Loading..."
-                },
-            // "deferLoading": 57,
-            //"serverSide": true,
-            "data":data.summary.incidents ,
-            "columns": [
-                    {"data": "number", "orderable" : true},
-                    {"data": "createdDate","orderable" : false  },
-                    {"data": "assigned_to" ,"orderable" : false },
-                    {"data": "category" ,"orderable" : false},
-                    {"data": "priority" ,"orderable" : false},
-                    {"data": "shortDesc" ,"orderable" : false},
-                    // {"data": "workLog" ,"orderable" : false},
-                    
-                ]
-        });
+        "processing": true,
+            'language':{ 
+               "loadingRecords": "&nbsp;",
+               "processing": "Loading..."
+            },
+        "data":data.summary.incidents ,
+        "columns": [
+            {"data": "number", "orderable" : true},
+            {"data": "createdDate","orderable" : false  },
+            {"data": "assigned_to" ,"orderable" : false },
+            {"data": "category" ,"orderable" : false},
+            {"data": "priority" ,"orderable" : false},
+            //{"data": "shortDesc" ,"orderable" : false},
+            // {"data": "workLog" ,"orderable" : false},
+        ]
+    });
 }
 
 
