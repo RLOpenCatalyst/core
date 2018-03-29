f0c796fb8211b49a23b1784b60503570eaad15d0
rairitu9 <ritu.rai@relevancelab.com>
2018-03-29 15:14:07 +0530

    Email notification

---
+++ server/app/services/emailService.js
@@ -0,0 +1,40 @@
+var logger = require('_pr/logger')(module);
+var nodeMailer = require('nodemailer');
+var authEmail = require('_pr/model/dao/authemaildao');
+
+var schedulerService = module.exports = {
+    sendEmail: sendEmail
+};
+
+function sendEmail(msg) {
+    authEmail.findOne({
+        category: 'failedbot'
+    }, function (err, config) {
+        if(err) logger.error(err);
+        else {
+            var transporter = nodeMailer.createTransport({
+                host: config.smtpserver,
+                port: 25
+            });
+
+            var mailOptions = {
+                from: config.email,
+                to: 'ritu.rai@relevancelab.com',
+                subject: config.subject,
+                text: config.body+msg,
+                html: config.body+msg
+            };
+
+            transporter.sendMail(mailOptions, function(error, info) {
+                if (error) {
+                    logger.error(error);
+                } else {
+                    logger.info('Message sent: %s', info.messageId);
+                }
+            });
+        }
+    })
+}
+
+
+sendEmail();
