2fe24917f606c9626f53dd403dc86a39c1e9088f
VeeraRlab <veeramani.murugesan@relevancelab.com>
2019-01-22 15:22:23 +0530

    migration script for auditId as bot_id instead of mongo _id

--- server/app/db-script/botIdUpdate.js
+++ server/app/db-script/botIdUpdate.js
@@ -1,5 +1,8 @@
 
+//to Connect devops_new DB
 db = db.getSiblingDB('devops_new');
+
+// to  update auditId field as botId instead of mongo _id
 db.getCollection('audittrails').find().forEach(element => {
    print(element.auditId)
    try {
@@ -18,21 +21,62 @@ db.getCollection('audittrails').find().forEach(element => {
    
 });
 
-// db = db.getSiblingDB('devops_new');
-// db.getCollection('botaudittrailsummaries').find().forEach(element => {
-//    print(element.botID)
-//    try {
-//       botID=ObjectId(element.botID)
-//       bot=db.getCollection('bots').findOne({_id:botID});
-//    if(bot){
-//     print(bot.id)
-//     updatedAudit=db.getCollection('botaudittrailsummaries').update({botID:element.botID},{$set:{botID:bot.id}})
-//    }else{
-//     print('null');
-//    }
-//    }
-//    catch(err){
-//       print('already changed')
-//    }
-   
-// });
+
+// to  update auditId field as botId instead of mongo _id
+botIds=db.getCollection("bots").distinct("id");
+botIds.forEach(botId=>{
+   print('botId  '+botId);
+      ids =db.getCollection("logs").find({"log":{$regex:".*"+botId+".*i"}},{referenceId:1}).map(e=>{
+         try {
+            element=ObjectId(e.referenceId[0]);
+            return e.referenceId[0]
+         }catch(err){
+            return e.referenceId[1]
+         }
+      }).filter(item=>{
+         if(item != undefined || item != null ){
+            return true;
+         }
+      })
+      uniq = [...new Set(ids)]
+      if(!uniq || uniq.length==0){
+         print ('logs not found')
+      }else{     
+      for (i=0;i<uniq.length;i++){      
+            element=uniq[i] 
+            if(element.length==24){
+            print('element  ' +element);
+            updatedAudit=db.getCollection('audittrails').update({auditId:element},{$set:{auditId:botId}},{"multi":true})
+            updatedlogs=db.getCollection("logs").update({"referenceId":element},{$set:{"referenceId.$":botId}},{"multi":true})
+            print (updatedAudit);  
+            print (updatedlogs)
+            }else{
+               print(element +'is not bot _id')
+            }                          
+            
+         }
+      }
+})
+
+
+// to Remove expired bots(non reproducible) audits records
+auditIds=db.getCollection("audittrails").distinct("auditId").map(item=>{
+   try {
+      element=ObjectId(item);
+      return item
+   }catch(err){
+      print ('already updated')
+   }
+}).filter(item=>{
+   if(item != undefined || item != null ){
+      return true;
+   }
+})
+removed=db.getCollection('audittrails').remove({auditId:{$in:auditIds}})
+print (removed)
+
+
+
+
+
+
